00beb30a814e6c5c97bc1444f485a5fff776b6de
Gilbert Salazar <grrr104@hotmail.com>
2018-12-30 23:19:37 -0600

    basic wpf tutorial i followed to get a feel on how it works.

---
+++ Wpf_1/Wpf_1/MainWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Wpf_1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ApplyButton_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show($"The description is: {DescriptionTextBox.Text}");
+        }
+
+        private void ResetButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.AssemblyCheckBox.IsChecked = this.WeldCheckBox.IsChecked = this.PlasmaCheckBox.IsChecked = this.LaserCheckBox.IsChecked = this.PurchaseCheckBox.IsChecked =
+                this.LatheCheckBox.IsChecked = this.DrillCheckBox.IsChecked = this.FoldCheckBox.IsChecked = this.RollCheckBox.IsChecked = this.SawCheckBox.IsChecked = false;
+        }
+
+        private void CheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            this.LengthTextBox.Text += (string)((CheckBox)sender).Content;
+        }
+
+        private void CheckBox_Unchecked(object sender, RoutedEventArgs e)
+        {
+            //scan and find sender string and remove from textbox
+        }
+
+        private void FinishComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (this.NoteTextBox == null)
+                return;
+
+            var combo = (ComboBox)sender;
+            var value = (ComboBoxItem)combo.SelectedValue;
+
+            this.NoteTextBox.Text = (string)value.Content;
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            FinishComboBox_SelectionChanged(this.FinishComboBox, null);
+        }
+
+        private void SupNameTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            this.MassTextBox.Text = this.SupNameTextBox.Text;
+        }
+    }
+}
